2c4aa2019a85cd8963061c93f6a573beec921dc1
Danilo-Mladenovic <danilo.mladenovic999@gmail.com>
2022-11-21 17:24:57 +0100

    Convert the input string to uppercase.

--- rust-tutorial/src/main.rs
+++ rust-tutorial/src/main.rs
@@ -1,10 +1,15 @@
 fn main() {
-    println!("{}", remove_char("abracadabra"));
+    println!("{}", make_upper_case("abracadabra"));
 }
 
 // Codewars Kata's for leaning rust.
 //
-// Remove First and Last Character
-pub fn remove_char(s: &str) -> String {
+// Remove first and last character.
+fn remove_char(s: &str) -> String {
     s[1..s.len() - 1].to_string()
+}
+
+// Convert the input string to uppercase.
+fn make_upper_case(s: &str) -> String {
+    s.to_uppercase()
 }
